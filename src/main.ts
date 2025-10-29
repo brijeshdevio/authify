d518004629ba17949b37dc91f6a429a95d6bd295
Brijesh <softbrijeshio@gmail.com>
2025-10-29 20:47:55 +0530

    Setup Authify API

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap()
+  .then(() => {
+    console.log('Server started on port ' + process.env.PORT);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
