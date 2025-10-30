e9e2b3893f27cce7d397e37fbe3efaa58c43a273
Brijesh <softbrijeshio@gmail.com>
2025-10-30 09:35:33 +0530

    fix: added input validation

--- src/auth/dto/signup-auth.dto.ts
+++ src/auth/dto/signup-auth.dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
 export class SignUpAuthDto {
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsStrongPassword()
   password: string;
 }
